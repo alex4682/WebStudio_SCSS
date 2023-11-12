752ca265cf026263a17cdf026ba2613613d777f5
Smoliarenko Alex <smoliarenko.sasha@gmail.com>
2023-11-12 13:45:48 +0200

    added animations at opening of modal window

--- modal.js
+++ modal.js
@@ -4,12 +4,28 @@
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
+  const open = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal-animation]"),
+  };
+  const close = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal-animation]"),
+  };
+  refs.openModalBtn.addEventListener("click", toggleModalOpen);
+  refs.closeModalBtn.addEventListener("click", toggleModalClose);
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+  function toggleModalOpen() {
     document.body.classList.toggle("no-scroll");
+    refs.modal.classList.toggle("is-hidden");
+    open.modal.classList.toggle("open");
+  }
+  function toggleModalClose() {
+    document.body.classList.toggle("no-scroll");
+    refs.modal.classList.toggle("is-hidden");
+    open.modal.classList.toggle("open");
+    close.modal.classList.toggle("close");
   }
 })();
